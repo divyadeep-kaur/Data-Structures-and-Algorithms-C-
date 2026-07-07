41a7165ad8e0e34368a4a151666a06631a830ac6
divyadeep-kaur <officialdivyadeep@gmail.com>
2026-07-07 13:26:10 +0530

    prime numbers

---
+++ basic math/hcf.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//O(min(n1,n2))
+void hcf(int n1 , int n2){
+    for(int i = min(n1,n2); i >= 1; i-- ){
+        if(n1%i==0 && n2%i==0){
+            cout<<i;
+            break;
+        }
+    }
+}
+int main(){
+    hcf(11,13);
+    return 0;
+}
--- basic math/sort.cpp
+++ basic math/sort.cpp
@@ -4,7 +4,6 @@ using namespace std;
 void sortedfac(int n){
     // 2 3 5 7 9 11.....
     vector<int>ls;
-    int count = 0;
     for( int i = 1 ; i*i <= n ; i++){
         if(n % i == 0){
             ls.push_back(i);
@@ -13,6 +12,7 @@ void sortedfac(int n){
             }
         }
     }
+    //O(nlogn)
     
     sort(ls.begin(),ls.end());
     for(auto it : ls)cout<<it<<" ";
