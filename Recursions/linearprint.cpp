721533455f7fbdd235e48830ce6b593b2dafd828
divyadeep-kaur <officialdivyadeep@gmail.com>
2026-07-10 04:33:06 +0530

    backtracking

--- Recursions/linearprint.cpp
+++ Recursions/linearprint.cpp
@@ -6,11 +6,20 @@ void func(int i , int n){
     }
     cout<<i<<" ";
     func(i+1,n);
+    cout<<endl;
+}
+void fun1(int i , int n){ //backtracking
+    if ( i < 1){
+        return ;
+    }
+    fun1(i - 1, n);
+    cout<<i<<" ";
 }
 int main(){
     int n;
     cout<<"how many times you want to print n numbers "<<endl;
     cin>>n;
     func(1,n);
+    fun1(n,n);
     return 0;
 }
