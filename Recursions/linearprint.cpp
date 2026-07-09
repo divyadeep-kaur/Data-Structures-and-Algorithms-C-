3c53f2990c3872a052d34c0b0a62da2951f3b717
divyadeep-kaur <officialdivyadeep@gmail.com>
2026-07-10 04:12:05 +0530

    counting recursions

---
+++ Recursions/linearprint.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void func(int i , int n){
+    if( i > n){
+        return ;
+    }
+    cout<<i<<" ";
+    func(i+1,n);
+}
+int main(){
+    int n;
+    cout<<"how many times you want to print n numbers "<<endl;
+    cin>>n;
+    func(1,n);
+    return 0;
+}
