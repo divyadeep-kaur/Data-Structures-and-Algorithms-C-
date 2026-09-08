4d5503ea424ae0ce8b77a78c81c2aa421e438689
divyadeep-kaur <officialdivyadeep@gmail.com>
2026-09-08 23:13:12 +0530

    practice of dsa revision

--- Practice/practice.cpp
+++ Practice/practice.cpp
@@ -1,28 +1,90 @@
 #include<iostream>
 using namespace std;
-
-int main(){
-    int n;
-    cout<<"enter the number of elements to add in an array "<<endl;
-    cin>>n;
-    int arr[n];
-    for ( int i = 0 ; i < n ; i++ ){
-        cout<<"enter the element "<<n<<" :"<<endl;
-        cin>>arr[i];
+int main(){//7789 --> 9877
+    int num;
+    int rev = 0;
+    cout<<"enter number to be reversed : "<<endl;
+    cin>>num;
+    while(num>0){
+        int digit = num%10;
+        num=num/10;
+        rev=(rev*10)+digit;
     }
-    int hash[10]={0};
-    for (int i = 0; i < n; i++ ){
-        hash[arr[i]]++;
-    }
-    int q;
-    cout<<"enter the number of queries you have "<<endl;
-    cin>>q;
-    while(q--){
-        int number ;
-        cout<<"enter the "<<q+1<<"th query"<<endl;
-        cin>>number;
-        cout<<endl;
-        cout<<"the value of query is "<<hash[number]<<endl;
-    }
-    return 0;
+    cout<<rev;
 }
+// int main(){
+//     for( int i = 1; i<= 4 ; i++){
+//         for(int j = 1 ; j<= i ; j++){
+//             cout<<j;
+//         }
+//         for(int space = 1;space<=2*(4-i);space++){
+//         cout<<" ";
+//         }
+//         for(int j2 = i;j2>=1;j2--){
+//             cout<<j2;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// int main(){
+//     for(int i = 1; i<= 5 ; i++){
+//         for(int j = 1; j<= i ; j++){
+//             if(i % 2 != 0){
+//                 if(j == 2 || j == 4){
+//                     cout<<'0';
+//                 }
+//                 else{
+//                     cout<<'1';
+//                 }
+//             }
+//             else{ //i = 2,4
+//                 if(j == 2 || j == 4){
+//                     cout<<'1';
+//                 }
+//                 else{
+//                     cout<<'0';
+//                 }
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+/*
+    *
+   ***
+  *****
+ *******
+*********
+*/
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter the number of elements to add in an array "<<endl;
+//     cin>>n;
+//     int arr[n];
+//     for ( int i = 0 ; i < n ; i++ ){
+//         cout<<"enter the element "<<n<<" :"<<endl;
+//         cin>>arr[i];
+//     }
+//     int hash[10]={0};
+//     for (int i = 0; i < n; i++ ){
+//         hash[arr[i]]++;
+//     }
+//     int q;
+//     cout<<"enter the number of queries you have "<<endl;
+//     cin>>q;
+//     while(q--){
+//         int number ;
+//         cout<<"enter the "<<q+1<<"th query"<<endl;
+//         cin>>number;
+//         cout<<endl;
+//         cout<<"the value of query is "<<hash[number]<<endl;
+//     }
+//     return 0;
+// }
