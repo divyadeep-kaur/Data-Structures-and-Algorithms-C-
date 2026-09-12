15ba0b9226b94d55ce6ae79a4733d8e6167759cb
divyadeep-kaur <officialdivyadeep@gmail.com>
2026-09-12 18:37:32 +0530

    iteration in map hashing

--- Hashing/map.cpp
+++ Hashing/map.cpp
@@ -2,10 +2,12 @@
 #include<map>
 using namespace std;
 int main(){
+    //number of elements in array 
     int n;
     cout<<"Hey!!how many elements you want to add in array? "<<endl;
     cin>>n;
 
+    //enter elements in array
     int arr[n];
     for (int i = 0; i < n; i++){
         cout<<"enter element of array: ";
@@ -16,7 +18,10 @@ int main(){
     for(int i=0 ; i<n ; i++){
         mpp[arr[i]]++;
     }
-
+    //lets see how mapping requires less memory then array by iterating
+    for( auto it : mpp){
+        cout<<it.first<<" -> "<<it.second<<endl;
+    }
     int q;
     cout<<"how many elements you want to find "<<endl;
     cin>>q;
