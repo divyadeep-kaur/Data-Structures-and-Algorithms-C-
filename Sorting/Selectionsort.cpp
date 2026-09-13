cc149fd645868d80e6d5712f16ae8f79e63c29f7
divyadeep-kaur <officialdivyadeep@gmail.com>
2026-09-13 14:03:38 +0530

    selection sort

---
+++ Sorting/Selectionsort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={12,3,5,6,4,9};
+    int n = 6;
+    for(int i = 0; i< n-1; i++){
+        int minIndex = i;
+        for(int j = i+1 ; j<n ; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(arr[i],arr[minIndex]);
+    }
+    for(int i = 0; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
